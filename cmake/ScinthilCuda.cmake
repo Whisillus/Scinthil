05ed3c71422d5b9fd289bce28080be40a1f1abc3
whisillus <whisillus@gmail.com>
2026-06-17 22:28:41 +0800

    Emit per-architecture CUDA dumps

--- cmake/ScinthilCuda.cmake
+++ cmake/ScinthilCuda.cmake
@@ -40,40 +40,60 @@ function(scinthil_add_cuda_dump_artifacts target_name ptx_enabled sass_enabled a
         "CUDA_PATH, or disable ${ptx_enabled} and ${sass_enabled}.")
   endif()
 
+  get_target_property(cuda_architectures ${target_name} CUDA_ARCHITECTURES)
+  if(NOT cuda_architectures)
+    set(cuda_architectures ${CMAKE_CUDA_ARCHITECTURES})
+  endif()
+
   set(artifact_dir "${CMAKE_CURRENT_BINARY_DIR}/${target_name}.cuda")
-  set(ptx_output "${artifact_dir}/${target_name}.ptx")
-  set(sass_output "${artifact_dir}/${target_name}.sass")
   set(capture_script "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ScinthilCaptureCudaDump.cmake")
 
   set(cuda_artifact_outputs)
 
-  if(${ptx_enabled})
-    add_custom_command(
-      OUTPUT "${ptx_output}"
-      COMMAND
-        "${CMAKE_COMMAND}" "-DSCINTHIL_CUDA_DUMP_TOOL=${SCINTHIL_CUOBJDUMP_EXECUTABLE}"
-        -DSCINTHIL_CUDA_DUMP_MODE=--dump-ptx
-        "-DSCINTHIL_CUDA_DUMP_INPUT=$<TARGET_FILE:${target_name}>"
-        "-DSCINTHIL_CUDA_DUMP_OUTPUT=${ptx_output}" -P "${capture_script}"
-      DEPENDS ${target_name}
-      VERBATIM
-      COMMENT "Writing CUDA PTX dump for ${target_name}")
-    list(APPEND cuda_artifact_outputs "${ptx_output}")
-  endif()
+  foreach(cuda_architecture IN LISTS cuda_architectures)
+    if(NOT cuda_architecture MATCHES "^([0-9]+[a-z]?)(-(real|virtual))?$")
+      message(
+        FATAL_ERROR
+          "CUDA dump artifacts for ${artifact_label} '${target_name}' require numeric CUDA_ARCHITECTURES, "
+          "but found '${cuda_architecture}'.")
+    endif()
 
-  if(${sass_enabled})
-    add_custom_command(
-      OUTPUT "${sass_output}"
-      COMMAND
-        "${CMAKE_COMMAND}" "-DSCINTHIL_CUDA_DUMP_TOOL=${SCINTHIL_CUOBJDUMP_EXECUTABLE}"
-        -DSCINTHIL_CUDA_DUMP_MODE=--dump-sass
-        "-DSCINTHIL_CUDA_DUMP_INPUT=$<TARGET_FILE:${target_name}>"
-        "-DSCINTHIL_CUDA_DUMP_OUTPUT=${sass_output}" -P "${capture_script}"
-      DEPENDS ${target_name}
-      VERBATIM
-      COMMENT "Writing CUDA SASS dump for ${target_name}")
-    list(APPEND cuda_artifact_outputs "${sass_output}")
-  endif()
+    set(cuda_architecture_name "sm_${CMAKE_MATCH_1}")
+    set(cuda_architecture_label "sm${CMAKE_MATCH_1}")
+    set(cuda_architecture_kind "${CMAKE_MATCH_3}")
+    set(ptx_output "${artifact_dir}/${target_name}.${cuda_architecture_label}.ptx")
+    set(sass_output "${artifact_dir}/${target_name}.${cuda_architecture_label}.sass")
+
+    if(${ptx_enabled} AND NOT cuda_architecture_kind STREQUAL "real")
+      add_custom_command(
+        OUTPUT "${ptx_output}"
+        COMMAND
+          "${CMAKE_COMMAND}" "-DSCINTHIL_CUDA_DUMP_TOOL=${SCINTHIL_CUOBJDUMP_EXECUTABLE}"
+          -DSCINTHIL_CUDA_DUMP_MODE=--dump-ptx
+          "-DSCINTHIL_CUDA_DUMP_ARCHITECTURE=${cuda_architecture_name}"
+          "-DSCINTHIL_CUDA_DUMP_INPUT=$<TARGET_FILE:${target_name}>"
+          "-DSCINTHIL_CUDA_DUMP_OUTPUT=${ptx_output}" -P "${capture_script}"
+        DEPENDS ${target_name}
+        VERBATIM
+        COMMENT "Writing CUDA PTX dump for ${target_name} ${cuda_architecture_name}")
+      list(APPEND cuda_artifact_outputs "${ptx_output}")
+    endif()
+
+    if(${sass_enabled} AND NOT cuda_architecture_kind STREQUAL "virtual")
+      add_custom_command(
+        OUTPUT "${sass_output}"
+        COMMAND
+          "${CMAKE_COMMAND}" "-DSCINTHIL_CUDA_DUMP_TOOL=${SCINTHIL_CUOBJDUMP_EXECUTABLE}"
+          -DSCINTHIL_CUDA_DUMP_MODE=--dump-sass
+          "-DSCINTHIL_CUDA_DUMP_ARCHITECTURE=${cuda_architecture_name}"
+          "-DSCINTHIL_CUDA_DUMP_INPUT=$<TARGET_FILE:${target_name}>"
+          "-DSCINTHIL_CUDA_DUMP_OUTPUT=${sass_output}" -P "${capture_script}"
+        DEPENDS ${target_name}
+        VERBATIM
+        COMMENT "Writing CUDA SASS dump for ${target_name} ${cuda_architecture_name}")
+      list(APPEND cuda_artifact_outputs "${sass_output}")
+    endif()
+  endforeach()
 
   add_custom_target(
     ${target_name}_cuda_artifacts ALL
