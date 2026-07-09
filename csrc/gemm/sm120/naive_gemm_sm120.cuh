7f7b51d15baca487eafd4cc65a133a5bfa79d858
whisillus <whisillus@gmail.com>
2026-07-09 13:38:19 +0800

    Refactor SM120 naive GEMM profile

--- csrc/gemm/sm120/naive_gemm_sm120.cuh
+++ csrc/gemm/sm120/naive_gemm_sm120.cuh
@@ -13,114 +13,117 @@ namespace scinthil::gemm::sm120 {
 
 using namespace cute;
 
-template <class ElementAB_, class ElementD_, class StrideA_, class StrideB_, class StrideD_>
-struct NaiveGEMMKernelSM120 {
-  using ElementAB = ElementAB_;
-  using ElementD = ElementD_;
-  using StrideA = StrideA_;
-  using StrideB = StrideB_;
-  using StrideD = StrideD_;
+template <class ElementAB, class SmemLayoutA, class SmemLayoutB>
+struct NaiveGEMMSM120SharedStorage {
+  alignas(128) ArrayEngine<ElementAB, cosize_v<SmemLayoutA>> a;
+  alignas(128) ArrayEngine<ElementAB, cosize_v<SmemLayoutB>> b;
+};
 
-  struct Params {
-    const ElementAB* a;
-    const ElementAB* b;
-    ElementD* d;
-
-    int m;
-    int n;
-    int k;
-
-    StrideA stride_a;
-    StrideB stride_b;
-    StrideD stride_d;
-  };
-
+template <class ElementAB, class ElementD, class StrideA, class StrideB, class StrideD>
+__launch_bounds__(32, 1) __global__
+    void naive_gemm_kernel_sm120(const ElementAB* a, const ElementAB* b, ElementD* d, int m, int n, int k,
+                                 StrideA stride_a, StrideB stride_b, StrideD stride_d) {
   using TileShape = Shape<Int<16>, Int<8>, Int<16>>;
   using SmemLayoutA = decltype(make_layout(select<0, 2>(TileShape{}), make_stride(size<2>(TileShape{}), _1{})));
   using SmemLayoutB = decltype(make_layout(select<1, 2>(TileShape{}), make_stride(size<2>(TileShape{}), _1{})));
-
-  struct SharedStorage {
-    alignas(128) ArrayEngine<ElementAB, cosize_v<SmemLayoutA>> a;
-    alignas(128) ArrayEngine<ElementAB, cosize_v<SmemLayoutB>> b;
-  };
-
-  static constexpr int MaxThreadsPerBlock{32};
-  static constexpr int MinBlocksPerMultiprocessor{1};
-  static constexpr int smem_size{static_cast<int>(sizeof(SharedStorage))};
-
+  using Storage = NaiveGEMMSM120SharedStorage<ElementAB, SmemLayoutA, SmemLayoutB>;
   using MmaOp = typename MMAOpSelectorRRSM120<ElementAB, ElementAB, ElementD, size<0>(TileShape{}),
                                               size<1>(TileShape{}), size<2>(TileShape{})>::Type;
+  constexpr int threads_per_block{32};
 
-  __device__ void operator()(Params const& params, char* shared_memory) const {
-    auto problem_shape = make_shape(params.m, params.n, params.k);
+  auto problem_shape = make_shape(m, n, k);
 
-    Tensor mA = make_tensor(make_gmem_ptr(params.a), select<0, 2>(problem_shape), params.stride_a);
-    Tensor mB = make_tensor(make_gmem_ptr(params.b), select<1, 2>(problem_shape), params.stride_b);
-    Tensor mD = make_tensor(make_gmem_ptr(params.d), select<0, 1>(problem_shape), params.stride_d);
+  Tensor mA = make_tensor(make_gmem_ptr(a), select<0, 2>(problem_shape), stride_a);
+  Tensor mB = make_tensor(make_gmem_ptr(b), select<1, 2>(problem_shape), stride_b);
+  Tensor mD = make_tensor(make_gmem_ptr(d), select<0, 1>(problem_shape), stride_d);
 
-    auto cta_coord = make_coord(blockIdx.x, blockIdx.y, _);
-    Tensor gA = local_tile(mA, TileShape{}, cta_coord, Step<_1, X, _1>{});  // (TileM, TileK, k_tile)
-    Tensor gB = local_tile(mB, TileShape{}, cta_coord, Step<X, _1, _1>{});  // (TileN, TileK, k_tile)
-    Tensor gD = local_tile(mD, TileShape{}, cta_coord, Step<_1, _1, X>{});  // (TileM, TileN)
+  auto cta_coord = make_coord(blockIdx.x, blockIdx.y, _);
+  Tensor gA = local_tile(mA, TileShape{}, cta_coord, Step<_1, X, _1>{});  // (TileM, TileK, k_tile)
+  Tensor gB = local_tile(mB, TileShape{}, cta_coord, Step<X, _1, _1>{});  // (TileN, TileK, k_tile)
+  Tensor gD = local_tile(mD, TileShape{}, cta_coord, Step<_1, _1, X>{});  // (TileM, TileN)
 
-    SharedStorage& storage = *reinterpret_cast<SharedStorage*>(shared_memory);
-    Tensor sA = make_tensor(make_smem_ptr(storage.a.begin()), SmemLayoutA{});
-    Tensor sB = make_tensor(make_smem_ptr(storage.b.begin()), SmemLayoutB{});
+  __shared__ Storage storage;
+  Tensor sA = make_tensor(make_smem_ptr(storage.a.begin()), SmemLayoutA{});
+  Tensor sB = make_tensor(make_smem_ptr(storage.b.begin()), SmemLayoutB{});
 
-    TiledCopy tiled_copy_a = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, ElementAB>{},
-                                             Layout<Shape<_16, _2>, Stride<_2, _1>>{}, Layout<Shape<_1, _8>>{});
-    TiledCopy tiled_copy_b = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<std::uint64_t>, ElementAB>{},
-                                             Layout<Shape<_8, _4>, Stride<_4, _1>>{}, Layout<Shape<_1, _4>>{});
+  TiledCopy tiled_copy_a = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, ElementAB>{},
+                                           Layout<Shape<_16, _2>, Stride<_2, _1>>{}, Layout<Shape<_1, _8>>{});
+  TiledCopy tiled_copy_b = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<std::uint64_t>, ElementAB>{},
+                                           Layout<Shape<_8, _4>, Stride<_4, _1>>{}, Layout<Shape<_1, _4>>{});
 
-    ThrCopy thr_copy_a = tiled_copy_a.get_slice(threadIdx.x);
-    Tensor tAgA = thr_copy_a.partition_S(gA);
-    Tensor tAsA = thr_copy_a.partition_D(sA);
+  ThrCopy thr_copy_a = tiled_copy_a.get_slice(threadIdx.x);
+  Tensor tAgA = thr_copy_a.partition_S(gA);
+  Tensor tAsA = thr_copy_a.partition_D(sA);
 
-    ThrCopy thr_copy_b = tiled_copy_b.get_slice(threadIdx.x);
-    Tensor tBgB = thr_copy_b.partition_S(gB);
-    Tensor tBsB = thr_copy_b.partition_D(sB);
+  ThrCopy thr_copy_b = tiled_copy_b.get_slice(threadIdx.x);
+  Tensor tBgB = thr_copy_b.partition_S(gB);
+  Tensor tBsB = thr_copy_b.partition_D(sB);
 
-    TiledMMA tiled_mma = make_tiled_mma(MmaOp{});
-    CUTE_STATIC_ASSERT_V(size(tiled_copy_a) == size(tiled_mma));
-    CUTE_STATIC_ASSERT_V(size(tiled_copy_b) == size(tiled_mma));
+  TiledMMA tiled_mma = make_tiled_mma(MmaOp{});
+  CUTE_STATIC_ASSERT_V(size(tiled_copy_a) == size(tiled_mma));
+  CUTE_STATIC_ASSERT_V(size(tiled_copy_b) == size(tiled_mma));
+  CUTE_STATIC_ASSERT_V(size(tiled_mma) == Int<threads_per_block>{});
 
-    ThrMMA thr_mma = tiled_mma.get_slice(threadIdx.x);
-    Tensor tDgD = thr_mma.partition_C(gD);
-    Tensor tCrA = thr_mma.partition_fragment_A(sA);
-    Tensor tCrB = thr_mma.partition_fragment_B(sB);
-    Tensor tCrC = thr_mma.make_fragment_C(tDgD);
-    clear(tCrC);
+  ThrMMA thr_mma = tiled_mma.get_slice(threadIdx.x);
+  Tensor tDgD = thr_mma.partition_C(gD);
+  Tensor tCrA = thr_mma.partition_fragment_A(sA);
+  Tensor tCrB = thr_mma.partition_fragment_B(sB);
+  Tensor tCrC = thr_mma.make_fragment_C(tDgD);
+  clear(tCrC);
 
-    Copy_Atom<SM75_U32x4_LDSM_N, ElementAB> s2r_atom_a;
-    Copy_Atom<SM75_U32x2_LDSM_N, ElementAB> s2r_atom_b;
-    TiledCopy tiled_s2r_copy_a = make_tiled_copy_A(s2r_atom_a, tiled_mma);
-    TiledCopy tiled_s2r_copy_b = make_tiled_copy_B(s2r_atom_b, tiled_mma);
+  Copy_Atom<SM75_U32x4_LDSM_N, ElementAB> s2r_atom_a;
+  Copy_Atom<SM75_U32x2_LDSM_N, ElementAB> s2r_atom_b;
+  TiledCopy tiled_s2r_copy_a = make_tiled_copy_A(s2r_atom_a, tiled_mma);
+  TiledCopy tiled_s2r_copy_b = make_tiled_copy_B(s2r_atom_b, tiled_mma);
 
-    ThrCopy s2r_thr_copy_a = tiled_s2r_copy_a.get_slice(threadIdx.x);
-    Tensor tXsA = s2r_thr_copy_a.partition_S(sA);
-    Tensor tXrA = s2r_thr_copy_a.retile_D(tCrA);
+  ThrCopy s2r_thr_copy_a = tiled_s2r_copy_a.get_slice(threadIdx.x);
+  Tensor tXsA = s2r_thr_copy_a.partition_S(sA);
+  Tensor tXrA = s2r_thr_copy_a.retile_D(tCrA);
 
-    ThrCopy s2r_thr_copy_b = tiled_s2r_copy_b.get_slice(threadIdx.x);
-    Tensor tXsB = s2r_thr_copy_b.partition_S(sB);
-    Tensor tXrB = s2r_thr_copy_b.retile_D(tCrB);
+  ThrCopy s2r_thr_copy_b = tiled_s2r_copy_b.get_slice(threadIdx.x);
+  Tensor tXsB = s2r_thr_copy_b.partition_S(sB);
+  Tensor tXrB = s2r_thr_copy_b.retile_D(tCrB);
 
-    const int k_tiles = params.k / size<2>(TileShape{});
-    for (int k_tile = 0; k_tile < k_tiles; ++k_tile) {
-      copy(tiled_copy_a, tAgA(_, _, _, k_tile), tAsA);
-      copy(tiled_copy_b, tBgB(_, _, _, k_tile), tBsB);
-      cp_async_fence();
-      cp_async_wait<0>();
-      __syncwarp();
+  const int k_tiles = k / size<2>(TileShape{});
+  for (int k_tile = 0; k_tile < k_tiles; ++k_tile) {
+    copy(tiled_copy_a, tAgA(_, _, _, k_tile), tAsA);
+    copy(tiled_copy_b, tBgB(_, _, _, k_tile), tBsB);
+    cp_async_fence();
+    cp_async_wait<0>();
+    __syncwarp();
 
-      copy(s2r_atom_a, tXsA, tXrA);
-      copy(s2r_atom_b, tXsB, tXrB);
-      cute::gemm(tiled_mma, tCrA, tCrB, tCrC);
+    copy(s2r_atom_a, tXsA, tXrA);
+    copy(s2r_atom_b, tXsB, tXrB);
+    cute::gemm(tiled_mma, tCrA, tCrB, tCrC);
 
-      __syncwarp();
-    }
-
-    copy(tCrC, tDgD);
+    __syncwarp();
   }
-};
+
+  copy(tCrC, tDgD);
+}
+
+template <class ElementAB, class ElementD, class StrideA, class StrideB, class StrideD>
+[[nodiscard]] cudaError_t launch_naive_gemm_sm120(const ElementAB* a, const ElementAB* b, ElementD* d, int m, int n,
+                                                  int k, StrideA stride_a, StrideB stride_b, StrideD stride_d,
+                                                  cudaStream_t stream = nullptr) {
+  auto tile_shape = make_shape(Int<16>{}, Int<8>{}, Int<16>{});
+
+  if (m <= 0 || n <= 0 || k <= 0) {
+    return cudaErrorInvalidValue;
+  }
+  if (a == nullptr || b == nullptr || d == nullptr) {
+    return cudaErrorInvalidValue;
+  }
+  if ((m % size<0>(tile_shape)) != 0 || (n % size<1>(tile_shape)) != 0 || (k % size<2>(tile_shape)) != 0) {
+    return cudaErrorInvalidValue;
+  }
+
+  dim3 grid(static_cast<unsigned>(m / size<0>(tile_shape)), static_cast<unsigned>(n / size<1>(tile_shape)));
+
+  naive_gemm_kernel_sm120<ElementAB, ElementD, StrideA, StrideB, StrideD>
+      <<<grid, 32, 0, stream>>>(a, b, d, m, n, k, stride_a, stride_b, stride_d);
+
+  return cudaGetLastError();
+}
 
 }  // namespace scinthil::gemm::sm120
---
+++ profile/gemm/sm120/profile_naive_gemm_sm120.cu
@@ -0,0 +1,124 @@
+#include <cuda_runtime.h>
+
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cute/numeric/numeric_types.hpp>
+#include <cute/tensor.hpp>
+
+#include "gemm/sm120/naive_gemm_sm120.cuh"
+#include "utils/utils.cuh"
+
+namespace scinthil::profile::gemm::sm120 {
+
+[[nodiscard]] bool profile_naive_gemm_sm120_fp16_fp16() {
+  constexpr int m{16};
+  constexpr int n{8};
+  constexpr int k{16};
+
+  constexpr auto a_bytes = static_cast<std::size_t>(m * k * sizeof(cute::half_t));
+  constexpr auto b_bytes = static_cast<std::size_t>(n * k * sizeof(cute::half_t));
+  constexpr auto d_bytes = static_cast<std::size_t>(m * n * sizeof(cute::half_t));
+
+  cute::half_t* a{nullptr};
+  cute::half_t* b{nullptr};
+  cute::half_t* d{nullptr};
+
+  bool success{true};
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&a), a_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&b), b_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&d), d_bytes));
+
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(a, 0, a_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(b, 0, b_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(d, 0, d_bytes));
+
+  auto stride_a = cute::make_stride(k, cute::_1{});
+  auto stride_b = cute::make_stride(k, cute::_1{});
+  auto stride_d = cute::make_stride(n, cute::_1{});
+
+  if (success) {
+    const auto status = scinthil::gemm::sm120::launch_naive_gemm_sm120<cute::half_t, cute::half_t>(
+        a, b, d, m, n, k, stride_a, stride_b, stride_d);
+    success = SCINTHIL_CUDA_CHECK(status) && success;
+  }
+  success = success && SCINTHIL_CUDA_CHECK(cudaDeviceSynchronize());
+
+  bool cleanup_success{true};
+  if (a != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(a)) && cleanup_success;
+  }
+  if (b != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(b)) && cleanup_success;
+  }
+  if (d != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(d)) && cleanup_success;
+  }
+
+  return success && cleanup_success;
+}
+
+[[nodiscard]] bool profile_naive_gemm_sm120_fp16_fp32() {
+  constexpr int m{16};
+  constexpr int n{8};
+  constexpr int k{16};
+
+  constexpr auto a_bytes = static_cast<std::size_t>(m * k * sizeof(cute::half_t));
+  constexpr auto b_bytes = static_cast<std::size_t>(n * k * sizeof(cute::half_t));
+  constexpr auto d_bytes = static_cast<std::size_t>(m * n * sizeof(float));
+
+  cute::half_t* a{nullptr};
+  cute::half_t* b{nullptr};
+  float* d{nullptr};
+
+  bool success{true};
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&a), a_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&b), b_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&d), d_bytes));
+
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(a, 0, a_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(b, 0, b_bytes));
+  success = success && SCINTHIL_CUDA_CHECK(cudaMemset(d, 0, d_bytes));
+
+  auto stride_a = cute::make_stride(k, cute::_1{});
+  auto stride_b = cute::make_stride(k, cute::_1{});
+  auto stride_d = cute::make_stride(n, cute::_1{});
+
+  if (success) {
+    const auto status = scinthil::gemm::sm120::launch_naive_gemm_sm120<cute::half_t, float>(a, b, d, m, n, k, stride_a,
+                                                                                            stride_b, stride_d);
+    success = SCINTHIL_CUDA_CHECK(status) && success;
+  }
+  success = success && SCINTHIL_CUDA_CHECK(cudaDeviceSynchronize());
+
+  bool cleanup_success{true};
+  if (a != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(a)) && cleanup_success;
+  }
+  if (b != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(b)) && cleanup_success;
+  }
+  if (d != nullptr) {
+    cleanup_success = SCINTHIL_CUDA_CHECK(cudaFree(d)) && cleanup_success;
+  }
+
+  return success && cleanup_success;
+}
+
+}  // namespace scinthil::profile::gemm::sm120
+
+int main() {
+  using scinthil::profile::gemm::sm120::profile_naive_gemm_sm120_fp16_fp16;
+  using scinthil::profile::gemm::sm120::profile_naive_gemm_sm120_fp16_fp32;
+
+  const bool fp16_success = profile_naive_gemm_sm120_fp16_fp16();
+  const bool fp32_success = profile_naive_gemm_sm120_fp16_fp32();
+  const bool success = fp16_success && fp32_success;
+  if (success) {
+    std::printf("SM120 naive GEMM profile launched FP16 and FP32 output variants\n");
+    return EXIT_SUCCESS;
+  }
+
+  std::fprintf(stderr, "SM120 naive GEMM profile failed\n");
+  return EXIT_FAILURE;
+}
